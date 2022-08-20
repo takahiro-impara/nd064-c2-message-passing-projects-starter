f6861276060d65a667e49f055a7f24c2061adfa4
fr-koichi <takahiro.koichi@fastretailing.com>
2022-08-21 00:32:33 +0900

    mod consumer

--- modules/location-consumer/consumer.py
+++ modules/location-consumer/consumer.py
@@ -27,23 +27,15 @@ DB_NAME = os.environ["DB_NAME"]
 
 engine = create_engine(
     f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
-SessionClass = sessionmaker(engine)
-session = SessionClass()
+conn = engine.connect()
 
 for x in consumer:
     message = json.loads(x.value.decode('utf-8'))
-    new_location = Location()
-    new_location.person_id = message["person_id"]
-    new_location.id = message["id"]
-    new_location.creation_time = message["creation_time"]
-    new_location.coordinate = ST_Point(
-        message["latitude"], message["longitude"])
-    print(new_location)
-    validation_results: Dict = LocationSchema().validate(new_location)
-    if validation_results:
-        print(f"Unexpected data format in payload: {validation_results}")
-        raise Exception(f"Invalid payload: {validation_results}")
+    person_id = message["person_id"]
+    latitude = message["latitude"]
+    longitude = message["longitude "]
+    insert = "INSERT INTO location (person_id, coordinate) VALUES ({}, ST_Point({}, {}))" \
+        .format(person_id, latitude, longitude)
 
-    session.add(new_location)
-    session.commit()
-    print(new_location)
+    print(insert)
+    conn.execute(insert)
