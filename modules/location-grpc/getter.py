aa7ea345102f9796b12d0b06c462e5bc9993a740
fr-koichi <takahiro.koichi@fastretailing.com>
2022-08-20 15:36:21 +0900

    mod writter/getter

--- modules/location-grpc/getter.py
+++ modules/location-grpc/getter.py
@@ -8,8 +8,9 @@ Sample implementation of a writer that can be used to write messages to gRPC.
 
 print("Sending sample payload...")
 
-channel = grpc.insecure_channel("localhost:5005")
+channel = grpc.insecure_channel(
+    "udaconnect-location.staging.udacity.impara8.com:5005")
 stub = location_pb2_grpc.LocationServiceStub(channel)
 
-response = stub.Get(location_pb2.LocationID())
+response = stub.Get(location_pb2.LocationID(id=29))
 print(response)
--- modules/location-grpc/writter.py
+++ modules/location-grpc/writter.py
@@ -8,7 +8,8 @@ Sample implementation of a writer that can be used to write messages to gRPC.
 
 print("Sending sample payload...")
 
-channel = grpc.insecure_channel("localhost:5005")
+channel = grpc.insecure_channel(
+    "udaconnect-location.staging.udacity.impara8.com:5005")
 stub = location_pb2_grpc.LocationServiceStub(channel)
 
 # Update this with desired payload
@@ -22,3 +23,4 @@ location = location_pb2.LocationMessage(
 
 
 response = stub.Create(location)
+print(response)
