de234e82fd8717438f7fd38d1bbbbf2bff70018e
fr-koichi <takahiro.koichi@fastretailing.com>
2022-08-21 13:41:59 +0900

    Revert "refactor person"
    This reverts commit 4b51a3ba8d276c846de059bd68f6fe5ffe74c390.

--- modules/person/app/udaconnect/controllers.py
+++ modules/person/app/udaconnect/controllers.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
-from app.udaconnect.models import Person
+from app.udaconnect.models import Connection, Location, Person
 from app.udaconnect.schemas import (
     ConnectionSchema,
+    LocationSchema,
     PersonSchema,
 )
-from app.udaconnect.services import ConnectionService, PersonService
+from app.udaconnect.services import ConnectionService, LocationService, PersonService
 from flask import request
 from flask_accepts import accepts, responds
 from flask_restx import Namespace, Resource
@@ -14,6 +15,26 @@ from typing import Optional, List
 DATE_FORMAT = "%Y-%m-%d"
 
 api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
+
+
+# TODO: This needs better exception handling
+
+
+@api.route("/locations")
+@api.route("/locations/<location_id>")
+@api.param("location_id", "Unique ID for a given Location", _in="query")
+class LocationResource(Resource):
+    @accepts(schema=LocationSchema)
+    @responds(schema=LocationSchema)
+    def post(self) -> Location:
+        request.get_json()
+        location: Location = LocationService.create(request.get_json())
+        return location
+
+    @responds(schema=LocationSchema)
+    def get(self, location_id) -> Location:
+        location: Location = LocationService.retrieve(location_id)
+        return location
 
 
 @api.route("/persons")
@@ -50,8 +71,7 @@ class ConnectionDataResource(Resource):
         start_date: datetime = datetime.strptime(
             request.args["start_date"], DATE_FORMAT
         )
-        end_date: datetime = datetime.strptime(
-            request.args["end_date"], DATE_FORMAT)
+        end_date: datetime = datetime.strptime(request.args["end_date"], DATE_FORMAT)
         distance: Optional[int] = request.args.get("distance", 5)
 
         results = ConnectionService.find_contacts(
